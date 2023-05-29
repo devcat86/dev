7d0811be9b7dfcc9f2bc06c21f7b70650e0af418
devcat86 <cybdevteam@hotmail.com>
2023-05-29 18:49:35 +0500

    comments update

--- js.js
+++ js.js
@@ -1,4 +1,9 @@
 function check() {
-    var x = document.getElementById("example").value;
-    alert(x)
+    var x = document.getElementById("jsinput").value;
+    if(x.value == "1"){
+    document.getElementById('jsdiv').innerText = 2;
+    }
+    else{
+        document.getElementById('jsdiv').innerText = x;
+    }
 }
