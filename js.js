ac81593f9fd30d93cf71e370ba8235450de291a2
devcat86 <cybdevteam@hotmail.com>
2023-05-29 19:11:06 +0500

    fix

--- js.js
+++ js.js
@@ -1,9 +1,4 @@
 function check() {
     var x = document.getElementById("jsinput").value;
-    if(x.value == "1"){
-    document.getElementById('jsdiv').innerText = 2;
-    }
-    else{
-        document.getElementById('jsdiv').innerText = x;
-    }
+    document.getElementById('jsdiv').innerText = x;
 }
